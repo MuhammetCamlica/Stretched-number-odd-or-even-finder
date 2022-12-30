bc0906c8bf7c60ac09ccec2fc19f062197996345
Muhammet Ali Çamlıca <Mecatr60@gmail.com>
2022-12-30 10:42:22 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace tekmi.ciftmi;
+
+class Program
+
+
+{
+    static void Main(string[] args)
+
+    {
+        Console.WriteLine("SAYI GİRİNİZ..");
+        int girilensayi = int.Parse(Console.ReadLine());
+
+        if (girilensayi %2 == 0)
+         {
+            Console.WriteLine("ÇİFT SAYI GİRDİNİZ.");
+        }
+        else
+        {
+            Console.WriteLine("TEK SAYI GİRDİNİZ.");
+        }
+
+
+    }
+}
